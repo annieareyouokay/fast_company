aa6ec051437ead71840589248bc53a93d9db4bf4
straponjimmy <telecaster068@gmail.com>
2022-08-11 00:29:26 +1000

    Добавлена пагинация, ESlint и prettier

--- src/app/components/bookmark.jsx
+++ src/app/components/bookmark.jsx
@@ -1,11 +1,17 @@
-import React from "react";
+import React from 'react';
+import PropTypes from 'prop-types';
 
-const Bookmark = ({bookmark, handleToogleBookmark}) => (
+const Bookmark = ({ bookmark, handleToogleBookmark }) => (
   <button onClick={handleToogleBookmark}>
     <h2>
-      <i className={`bi bi-star${bookmark ? "-fill" : ""}`} />
+      <i className={`bi bi-star${bookmark ? '-fill' : ''}`} />
     </h2>
   </button>
 );
+
+Bookmark.propTypes = {
+  bookmark: PropTypes.bool.isRequired,
+  handleToogleBookmark: PropTypes.func.isRequired
+};
 
 export default Bookmark;
--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import "bootstrap/dist/css/bootstrap.css";
+import 'bootstrap/dist/css/bootstrap.css';
 import App from './app/app';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App/>
+    <App />
   </React.StrictMode>
 );
 
