5b6ad0a1f566a093ccea9338f7e8467181ac80ea
straponjimmy <telecaster068@gmail.com>
2022-11-03 14:42:32 +1000

    added firebase

--- src/app/services/http.service.js
+++ src/app/services/http.service.js
@@ -1,22 +1,40 @@
 import axios from 'axios';
 import { toast } from 'react-toastify';
-import config from '../config.json';
+import configFile from '../config.json';
 
-axios.defaults.baseURL = config.apiEndpoint;
+axios.defaults.baseURL = configFile.apiEndpoint;
 
-axios.interceptors.response.use((res) => res, function (error) {
-  const expectedErrors =
-    error.response &&
-    error.response.status >= 400 &&
-    error.response.status < 500;
+axios.interceptors.request.use(
+  function (config) {
+    if (configFile.isFireBase) {
+      const containSlash = /\/$/gi.test(config.url);
+      config.url =
+        (containSlash ? config.url.slice(0, -1) : config.url) + '.json';
+    }
 
-  if (!expectedErrors) {
-    console.log();
-    toast.error('Something went wrong. Try it later');
-  };
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
 
-  return Promise.reject(error);
-});
+axios.interceptors.response.use(
+  (res) => res,
+  function (error) {
+    const expectedErrors =
+      error.response &&
+      error.response.status >= 400 &&
+      error.response.status < 500;
+
+    if (!expectedErrors) {
+      console.log();
+      toast.error('Something went wrong. Try it later');
+    }
+
+    return Promise.reject(error);
+  }
+);
 
 const httpService = {
   get: axios.get,
